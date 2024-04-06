01f0c3a210c1130f18e37bf525fbb7d4c4557eeb
skylerlee <skyler.ac.lee@gmail.com>
2024-04-06 17:18:00 +0800

    feat: add xterm

--- src/renderer/app.ts
+++ src/renderer/app.ts
@@ -1,1 +1,5 @@
-document.write('<h1>Test</h1>');
+import { Terminal } from '@xterm/xterm';
+
+const term = new Terminal();
+term.open(document.getElementById('root'));
+term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
