cec9896327973261d4bf3b1cf119595095fb6e85
skylerlee <skyler.ac.lee@gmail.com>
2024-04-07 12:06:00 +0800

    feat: add solid jsx support

--- src/renderer/app.ts
+++
@@ -1,5 +0,0 @@
-import { Terminal } from '@xterm/xterm';
-
-const term = new Terminal();
-term.open(document.getElementById('root'));
-term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
---
+++ src/renderer/app.tsx
@@ -0,0 +1,8 @@
+import { render } from 'solid-js/web';
+
+const App = () => {
+  return <div>Hello</div>;
+};
+
+const root = document.getElementById('root');
+render(App, root);
