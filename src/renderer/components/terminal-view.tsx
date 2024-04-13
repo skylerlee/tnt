2610103797f10d5dd99c2259aef02b3a4d5a5a19
skylerlee <skyler.ac.lee@gmail.com>
2024-04-13 20:27:00 +0800

    feat: rename

---
+++ src/renderer/components/terminal-view.tsx
@@ -0,0 +1,27 @@
+import { FitAddon } from '@xterm/addon-fit';
+import { Terminal } from '@xterm/xterm';
+import { type Component, onCleanup, onMount } from 'solid-js';
+
+type IProps = { active?: boolean };
+
+const TerminalView: Component<IProps> = (props) => {
+  let parent: HTMLDivElement;
+
+  onMount(() => {
+    const terminal = new Terminal();
+    const fitAddon = new FitAddon();
+    terminal.loadAddon(fitAddon);
+    terminal.open(parent);
+    terminal.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+
+    // fit.fit();
+
+    onCleanup(() => {
+      terminal.dispose();
+    });
+  });
+
+  return <div class="terminal-view" ref={parent} />;
+};
+
+export default TerminalView;
