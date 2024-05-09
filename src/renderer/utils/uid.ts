575e8faadf1c030466c4dcebda5a9ab537533e16
skylerlee <skyler.ac.lee@gmail.com>
2024-05-10 00:28:09 +0800

    feat: add uid

---
+++ src/renderer/utils/uid.ts
@@ -0,0 +1,24 @@
+const POOL_SIZE_MULTIPLIER = 128;
+const ID_SIZE = 21;
+
+const pool = new Uint8Array(ID_SIZE * POOL_SIZE_MULTIPLIER);
+let poolOffset = 0;
+
+crypto.getRandomValues(pool);
+
+function fillPool() {
+  if (pool.length < poolOffset + ID_SIZE) {
+    crypto.getRandomValues(pool);
+    poolOffset = 0;
+  }
+  poolOffset += ID_SIZE;
+}
+
+export function uid() {
+  fillPool();
+  let id = 0;
+  for (let i = poolOffset - ID_SIZE; i < poolOffset; i++) {
+    id = (id << 8) + pool[i];
+  }
+  return id;
+}
