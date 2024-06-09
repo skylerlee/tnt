6b1b34e649101b9606f23b730da3bdb7cde042e7
skylerlee <skyler.ac.lee@gmail.com>
2024-06-09 12:21:26 +0800

    build: add vite config

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import solid from 'vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [solid()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+});
