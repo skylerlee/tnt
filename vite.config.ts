375b9021be0b0877f4dd1fc966f875dbc7b61f5e
skylerlee <skyler.ac.lee@gmail.com>
2024-06-09 23:44:27 +0800

    build: add electron plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
 import path from 'node:path';
 import { defineConfig } from 'vite';
+import electron from 'vite-plugin-electron/simple';
 import solid from 'vite-plugin-solid';
 
+const alias = [{ find: '~', replacement: path.resolve(__dirname, 'src') }];
+
 export default defineConfig({
-  plugins: [solid()],
-  resolve: {
-    alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
-  },
+  plugins: [
+    electron({
+      main: {
+        entry: 'src/main/index.ts',
+        vite: {
+          resolve: { alias },
+          build: {
+            rollupOptions: {
+              external: ['electron', 'node-pty', '@vscode/windows-process-tree'],
+            },
+          },
+        },
+      },
+      preload: {
+        input: 'src/preload/index.ts',
+        vite: {
+          resolve: { alias },
+        },
+      },
+    }),
+    solid(),
+  ],
+  resolve: { alias },
   css: {
     postcss: {},
   },
